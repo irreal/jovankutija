2151f22da36191b3826ecc842674dc68fbb48660
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-05-02 17:11:05 +0200

    setup angular, angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA_UkcJWoHkPJGRQk7o3Zh7EMeMfL57384',
+    authDomain: 'jovankutija.firebaseapp.com',
+    databaseURL: 'https://jovankutija.firebaseio.com',
+    projectId: 'jovankutija',
+    storageBucket: 'jovankutija.appspot.com',
+    messagingSenderId: '727202763217',
+    appId: '1:727202763217:web:a9c4ee06497d9f764110f0',
+    measurementId: 'G-9R9CPQEWZE',
+  },
 };
